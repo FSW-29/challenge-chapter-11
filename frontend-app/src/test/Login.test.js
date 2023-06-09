709f8add24a89b8f1e9a487e60d213147de52de6
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-06-09 14:22:28 +0700

    create test login page

--- frontend-app/src/test/Login.test.js
+++ frontend-app/src/test/Login.test.js
@@ -1,15 +1,57 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "..";
 import LoginPage from "../pages/Login.page";
 import React from "react";
+
+//eslint-disable-next-line
+jest.mock('axios');
 
 //eslint-disable-next-line
 describe('Testing Login Page', () => {
   //eslint-disable-next-line
   test('Render Login Page', () => {
-    render(<LoginPage />)
-    const loginComponentElement = screen.getByText(/Login/i);
+    render(
+      <Provider store={ store }>
+        <MemoryRouter>
+          <LoginPage />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const loginComponentElement = screen.getByText(/Login Page/i);
     //eslint-disable-next-line
     expect(loginComponentElement).toBeInTheDocument();
   });
-  
+
+  //eslint-disable-next-line
+  test('Test Find Button Login', () => {
+    render(
+      <Provider store={ store }>
+        <MemoryRouter>
+          <LoginPage />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const buttonLogin = screen.getByText(/Login Now/i);
+    //eslint-disable-next-line
+    expect(buttonLogin).toBeInTheDocument();
+  });
+
+  //eslint-disable-next-line
+  test('Test Find Button Register', () => {
+    render(
+      <Provider store={ store }>
+        <MemoryRouter>
+          <LoginPage />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const buttonLogin = screen.getByText(/Dont Have Account/i);
+    //eslint-disable-next-line
+    expect(buttonLogin).toBeInTheDocument();
+  });
 });
