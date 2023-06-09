6fb28138fcbaf115fba0b08d40488b2f0d942e48
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-06-09 14:21:21 +0700

    setup cors in backend

--- backend-app/app.js
+++ backend-app/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -10,6 +11,10 @@ const profileRouter = require('./routes/profile');
 const authRouter = require('./routes/auth');
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
