752a3f3aef1b3226a58bb878e0641ae0ae9ff948
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-06-09 16:16:04 +0700

    redux untuk register

--- frontend-app/src/redux/reducers/auth/auth.reducer.js
+++ frontend-app/src/redux/reducers/auth/auth.reducer.js
@@ -1,13 +1,19 @@
 import { 
   LOGIN_SUCCESS, 
   LOGIN_FAILED, 
-  LOGIN_LOADING 
+  LOGIN_LOADING,
+  REGISTER_SUCCESS,
+  REGISTER_FAILED,
+  REGISTER_LOADING
 } from "../../actions/auth.action";
 
 const initialState = {
   loginUserFulfilled: false,
   loginUserRejected: false,
-  loginUserLoading: false
+  loginUserLoading: false,
+  registerUserFulfilled: false,
+  registerUserRejected: false,
+  registerUserLoading: false,
 };
 
 const authReducer = (state = initialState, action) => {
@@ -29,6 +35,24 @@ const authReducer = (state = initialState, action) => {
       return {
         ...state,
         loginUserLoading: action.payload
+      }
+    case REGISTER_SUCCESS:
+      // console.info(action.payload, '=> dari case LOGIN_SUCCESS');
+      return {
+        ...state,
+        registerUserFulfilled: action.payload
+      }
+    case REGISTER_FAILED: 
+      // console.info(action.payload, '=> dari case LOGIN_FAILED');
+      return {
+        ...state,
+        registerUserRejected: action.payload
+      }
+    case REGISTER_LOADING:
+      // console.info(action.payload, '=> dari case LOGIN_LOADING');
+      return {
+        ...state,
+        registerUserLoading: action.payload
       }
     default:
       return state;
