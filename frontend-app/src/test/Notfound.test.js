7ce268f37431205025f375f5a92fb1dcc8a0ed52
Steven Aprullianto <stevenaprullianto@gmail.com>
2023-06-10 22:18:59 +0700

    fixing notfound test

--- frontend-app/src/test/Notfound.test.js
+++ frontend-app/src/test/Notfound.test.js
@@ -1,13 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import NotFoundPage from "../pages/notfound/notfound.jsx"
 import React from 'react';
+import { MemoryRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "..";
 
 
 //eslint-disable-next-line
 describe("TEST PAGE DOESNT EXIST /", () => {
 //eslint-disable-next-line
     test("Page Doest Exist", done => {
-        render(<NotFoundPage />);
+        render(<Provider store={ store }>
+                    <MemoryRouter>
+                        <NotFoundPage />
+                    </MemoryRouter>
+                </Provider>);
         const linkElement= screen.getByText(/404/i);
         //eslint-disable-next-line
         expect(linkElement).toBeInTheDocument();
