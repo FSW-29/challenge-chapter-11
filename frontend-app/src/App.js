5354b95180e69acfcac16c990fa2e693ae23ae30
Steven Aprullianto <stevenaprullianto@gmail.com>
2023-06-10 12:04:53 +0700

    Add game list and game detail

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -4,6 +4,8 @@ import Home from './pages/home/Home';
 import LoginPage from './pages/Login.page';
 import RegisterPage from './pages/Register.page';
 import UploadFile from './components/UploadFile';
+import Game from './pages/game/Game'
+import GameDetail from './pages/game/GameDetail';
 
 function App() {
   // const baseUrl = 'localhost:8000/' || process.env.BASE_URL;
@@ -16,6 +18,8 @@ function App() {
         <Route path='/register' element={ <RegisterPage /> } />
         <Route path='/login' element={ <LoginPage dataLogin={"Login Page"}/> } />
         <Route path='/upload' element={ <UploadFile /> } />
+        <Route path='/GameList' element={ <Game /> } />
+        <Route path='/GameDetail' element={ <GameDetail /> } />
       </Routes>
     </BrowserRouter>
   );
--- frontend-app/src/pages/home/Home.jsx
+++ frontend-app/src/pages/home/Home.jsx
@@ -1,9 +1,84 @@
 import React, { useEffect, useState } from 'react';
 import NavbarMainComponent from '../../components/NavbarMain.component';
+import axios from 'axios';
+import { useDispatch } from "react-redux";
+import {
+    gameList,
+    gameRacing,
+    gamePuzzle,
+    gameAction,
+    gameNew,
+    gameLeaderboard
+} from "../../redux/actions/game.action"
 // import { useDispatch, useSelector } from "react-redux";
 
+const base_url = "http://localhost:8000/";
 
 export default function HomePage(){
+    const dispatch = useDispatch();
+
+    const [game, setGame] = useState();
+    const [racing, setRacing] = useState();
+    const [puzzle, setPuzzle] = useState();
+    const [action, setAction] = useState();
+    const [gameTypeNew, setGameTypeNew] = useState();
+    const [leaderboard, setLeaderboard] = useState();
+
+    const getDataGame = async() =>{
+        const response = await axios.get(`${base_url}game`);
+        const dataGame = response.data.data_game;
+        const dataLeaderboard = response.data.data_leaderboard
+        setGame(dataGame);
+        setLeaderboard(dataLeaderboard);
+        manipulationTypeGame(response.data.data_game)
+        //console.log(game, "======> ini responses")
+    }
+
+    function manipulationTypeGame(dataGame) {
+        const typeRacing = [];
+        const typePuzzle = [];
+        const typeAction = [];
+        const typeNew = [];
+
+        dataGame.forEach((element) => {
+        if (element.type === "racing") {
+            typeRacing.push(element);
+        } else if (element.type === "puzzle") {
+            typePuzzle.push(element);
+        } else if (element.type === "action") {
+            typeAction.push(element);
+        } else if (element.type === "new") {
+            typeNew.push(element);
+        }
+        });
+
+        setRacing(typeRacing);
+        setPuzzle(typePuzzle);
+        setAction(typeAction);
+        setGameTypeNew(typeNew);
+    }
+    
+    useEffect(() => {
+        getDataGame();
+    }, []);
+
+    useEffect(() => {
+        racing;
+        const dataGameList = gameList(game);
+        const dataGameRacing = gameRacing(racing);
+        const dataGamePuzzle = gamePuzzle(puzzle);
+        const dataGameAction = gameAction(action);
+        const dataGameNew = gameNew(gameTypeNew);
+        const dataLeaderboard = gameLeaderboard(leaderboard);
+        console.log(dataLeaderboard, "===============> dataLeaderboard");
+
+        dispatch(dataGameList);
+        dispatch(dataGameRacing);
+        dispatch(dataGamePuzzle);
+        dispatch(dataGameAction);
+        dispatch(dataGameNew);
+        dispatch(dataLeaderboard);
+    }, [game]);
 
 
     const [validLogin, setValidLogin]=useState()
@@ -15,7 +90,7 @@ export default function HomePage(){
     const cekToken = () =>{
         if (localStorage.getItem("token")) {
             setValidLogin("Valid");
-          }
+        }
     }
 
     return(
